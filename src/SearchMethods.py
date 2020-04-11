f122cacbc4c99c684ba6f284bcecedbe3862aee1
diogofalken <diogofalken0@gmail.com>
2020-04-11 15:41:48 +0100

    :construction: Struture for the main search-methods done

---
+++ src/SearchMethods.py
@@ -0,0 +1,15 @@
+class SearchMethods:
+    def __init__(self, _cities):
+        super().cities = _cities
+
+    def uniformCostSearch(beginCity, endCity):
+        pass
+
+    def depthLimitedSearch(beginCity, endCity):
+        pass
+
+    def sofregaSearch(beginCity, endCity="Faro"):
+        pass
+
+    def aStarSearch(beginCity, endCity="Faro"):
+        pass
